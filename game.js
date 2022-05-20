890635c801ef405236d7e118770f2e5ef2b7158b
GeorgioNehme <georgionehme@gmail.com>
2022-05-20 18:16:50 +0300

    Finished the scoring still a small problem with the colors

--- game.js
+++ game.js
@@ -1,29 +1,47 @@
 window.onload = function() {
 
+    var score = 0
+
     document.getElementById("status").innerHTML="Put your mouse over the S to start the game."
 
-    document.getElementById("start").onmouseover = function() {start_game()}
+    document.getElementById("start").onclick = function() {start_game()}
 
+    document.getElementById("score").innerHTML= "Your Score: " + score; 
+
+
+
+    
     function start_game() {
 
         document.getElementById("status").innerHTML="To finish the game pass between the walls to reach the E "
    
         const bounds=document.getElementsByClassName("boundary")
 
-            for (var x = 0 ; x < bounds.length ; x++){
-                bounds[x].addEventListener("mouseover", you_lost) 
-            }
-
+		for (var x = 0 ; x < bounds.length ; x++){
+			bounds[x].addEventListener("mouseover", you_lost);
+			bounds[x].classList.remove("youlose"); 
+                       
+		}
+		
+        document.getElementById("end").addEventListener("mouseover", you_won)
+		
+        function you_won() {
+			if(document.getElementById("status").innerHTML!=="You Win" && document.getElementById("status").innerHTML!=="Try again"){
+				document.getElementById("status").innerHTML="You Win";
+				score = score + 5;
+				document.getElementById("score").innerHTML="Your Score: " + score;
+			}
+        }
     
         function you_lost() {
-            for (var x = 0 ; x < bounds.length ; x++){
-                bounds[x].style.backgroundColor = "red";
-                document.getElementById("status").innerHTML="Try again"
-            }
-   
+    
+                if(document.getElementById("status").innerHTML!=="You Win" && document.getElementById("status").innerHTML!=="Try again"){
+                    document.getElementById("status").innerHTML="You Win";
+                    score = score - 10;
+                    document.getElementById("score").innerHTML="Your Score: " + score;
+                }
         }
-            
+        
     }
-
-
+        
 }
