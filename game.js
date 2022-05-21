84ac71cc5376b7790ebb44678077fda352f812e8
GeorgioNehme <georgionehme@gmail.com>
2022-05-21 09:55:52 +0300

    Finalized the game

--- game.js
+++ game.js
@@ -1,29 +1,25 @@
 window.onload = function() {
 
-    var score = 0
-
-    document.getElementById("status").innerHTML="Put your mouse over the S to start the game."
+	var score = 0;
+    document.getElementById("status").innerHTML="Put your mouse over the S to start the game.";
+    document.getElementById("score").innerHTML="Your Score: " + score;
 
     document.getElementById("start").onclick = function() {start_game()}
 
-    document.getElementById("score").innerHTML= "Your Score: " + score; 
 
 
-
-    
     function start_game() {
-
-        document.getElementById("status").innerHTML="To finish the game pass between the walls to reach the E "
+		
+        document.getElementById("status").innerHTML="To finish the game pass between the walls to reach the E ";
    
-        const bounds=document.getElementsByClassName("boundary")
+        const bounds=document.getElementsByClassName("boundary");
 
 		for (var x = 0 ; x < bounds.length ; x++){
 			bounds[x].addEventListener("mouseover", you_lost);
-			bounds[x].classList.remove("youlose"); 
-                       
+			bounds[x].classList.remove("youlose");                
 		}
 		
-        document.getElementById("end").addEventListener("mouseover", you_won)
+        document.getElementById("end").addEventListener("mouseover", you_won);
 		
         function you_won() {
 			if(document.getElementById("status").innerHTML!=="You Win" && document.getElementById("status").innerHTML!=="Try again"){
@@ -34,12 +30,15 @@ window.onload = function() {
         }
     
         function you_lost() {
-    
-                if(document.getElementById("status").innerHTML!=="You Win" && document.getElementById("status").innerHTML!=="Try again"){
-                    document.getElementById("status").innerHTML="You Win";
-                    score = score - 10;
-                    document.getElementById("score").innerHTML="Your Score: " + score;
-                }
+			if(document.getElementById("status").innerHTML!=="You Win" && document.getElementById("status").innerHTML!=="Try again"){
+				for (var x = 0 ; x < bounds.length ; x++){
+					//bounds[x].style.backgroundColor = "red";
+					bounds[x].classList.add("youlose");
+					document.getElementById("status").innerHTML="Try again"; 
+				};
+				score = score - 10;
+				document.getElementById("score").innerHTML="Your Score: " + score;
+			}
         }
         
     }
